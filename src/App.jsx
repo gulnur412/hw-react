c0a9ea90fe093da490e92d66838930d5448e466f
gulnur <abdrashitovagulnur05@gmail.com>
2024-09-08 19:43:05 +0300

    finished

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Div } from "./components/Div";
+
+function App() {
+  return (
+    <div>
+      <Div />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Div.jsx
@@ -0,0 +1,94 @@
+import "./Div.css";
+
+export function Div() {
+  return (
+    <div className="div1">
+      <div className="container">
+        <h1 className="h1">трикодажные платье в полоску </h1>
+      </div>
+
+      <div className="coment">
+        <div>
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSG5jPtMUkdbdSZdnzGWCc2M_mOJa1J0qtehw&s"
+            alt=""
+          />
+        </div>
+        <div className="flex">
+          <div className="colors">
+            <p>Цвета в наличии</p>
+
+            <div className="class">
+              <div className="black"></div>
+              <div className="blue "></div>
+              <div className="aqua"></div>
+            </div>
+          </div>
+
+          <div className="colors">
+            <p>Размеры в наличии</p>
+
+            <p>S,M,L,XL,XXL</p>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div className="container">
+          <h1 className="h1">Платье</h1>
+        </div>
+
+        <div className="coment">
+          <div>
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR30oRZrLDR-xVKDoeeXmc_SQmskkhzGcOfDw&s"
+              alt=""
+            />
+          </div>
+          <div className="flex">
+            <div className="colors">
+              <p>Цвета в наличии</p>
+
+              <div className="class">
+                <div className="black"></div>
+                <div className="blue "></div>
+                <div className="aqua"></div>
+              </div>
+            </div>
+            <div className="colors">
+              <p>Размеры в наличии</p>
+
+              <p>S,M,L,XL,XXL</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <h1 className="h1">Вязанный топ</h1>
+      </div>
+
+      <div className="coment">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1NwWgt8KayfQ56q04L-oWtTC9w1-zx1THPg&s"
+          alt=""
+        />
+        <div className="flex">
+          <div className="colors">
+            <p>Цвета в наличии</p>
+
+            <div className="class">
+              <div className="black"></div>
+              <div className="blue "></div>
+              <div className="aqua"></div>
+            </div>
+          </div>
+          <div className="colors">
+            <p>Размеры в наличии</p>
+
+            <p>S,M,L,XL,XXL</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
